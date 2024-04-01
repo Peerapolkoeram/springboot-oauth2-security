0eb1ded8abc753a7833eea43fe188f739ce52c20
peerapolkoeram <peerapolkoeram@gmail.com>
2024-04-01 22:09:20 +0700

    create dao users

--- src/main/java/com/TPK/OAuth2Security/dao/RolesDao.java
+++ src/main/java/com/TPK/OAuth2Security/dao/RolesDao.java
@@ -1,4 +1,36 @@
 package com.TPK.OAuth2Security.dao;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class RolesDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long user_id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @Column(name = "create_by")
+    private String create_by;
+
 }
